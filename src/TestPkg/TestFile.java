40359c4249767677046d2d1c7ef2f474db25ee48
Maryna <mogneva78@gmail.com>
2020-10-05 21:56:16 -0400

    TestFile

--- src/TestPkg/TestFile.java
+++ src/TestPkg/TestFile.java
@@ -5,48 +5,14 @@ public class TestFile {
         System.out.println("Hello World!");
 
 
+        //Homework 5
 
-
-        double fTemp = 72;
-        double cTemp =(fTemp - 32) * 5/9;
-        System.out.println(fTemp + " F is equal to " + cTemp + " deg Celsius");
-
-
-        //  kT(K) = (T(°F) + 459.67)× 5/9
-
-        double oTemp = 97;
-        double kTemp =(oTemp + 459.67) * 5/9;
-        System.out.println(oTemp + " F is equal to" + kTemp + " deg Kelvin");
-
-
-        //  T(°C) = T(K) - 273.15
-
-        double cTem = 10;
-        double kTem =(cTem - 273.15);
-        System.out.println(kTem + " K is equal to " + cTem + " deg C ");
-
-        //  T(°F) = T(K) × 9/5 - 459.67
-        double kTe = 387;
-        double fTe =(kTe * 9/5 -( 459.67));
-        System.out.println(fTe + " F is equal to " + kTe + " deg K ");
-
-
-        // T(°F) = T(°C) × 9/5 + 32
-        double cT = 17;
-        double fT =((cT * 9/5) + 32);
-        System.out.println( cT + " C is equal to " + fT + " deg F ");
-
-
-        // T(K) = T(°C) + 273.15
-        double cTt = 15;
-        double kTt =(cTt + 273.15);
-        System.out.println(cTt + " C is equal to " + kTt + " deg K");
-
-
-
-
-
-
+        String[] msg = {"very good evening"};
+        String[] abr = {"vge"};
+        for (int i = 0; i <= msg.length - 1; i++) {
+            System.out.println(msg[i]);
+            System.out.println(abr[i]);
+        }
 
     }
 }
---
+++ src/TestPkg/Testing.java
@@ -0,0 +1,18 @@
+package TestPkg;
+
+public class Testing {
+    public static void main(String[] args) {
+        if (2>1) {
+            System.out.println("hello");
+        }
+
+        //HOMEWORK 5
+
+       char[] alphabets = {'a','b','c','d','d','e','f','g'};
+        for (int i=0; i<= alphabets.length-1; i++) {
+            System.out.println(alphabets[i]);
+        }
+
+
+    }
+}
