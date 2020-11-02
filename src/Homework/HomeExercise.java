7915a26bf554e44bf9bd2ac30e88e77c31f2f63c
Maryna <mogneva78@gmail.com>
2020-11-02 01:30:47 -0500

    Testing

--- src/Homework/HomeExercise.java
+++ src/Homework/HomeExercise.java
@@ -1,1 +1,26 @@
 
+/**ArrayList<Integer> arr2 = new ArrayList<>();
+//arr2.add(4);
+//arr2.add(6);
+//Scanner to = new Scanner(System.in);
+//int toGet = to.nextInt();
+//if(toGet>0){
+//    arr2.add(toGet);
+//}
+   /**ArrayList<String> arrList = new ArrayList();
+        // add values
+        arrList.add("happy");
+        arrList.add("peace");
+        arrList.add("grow");
+        arrList.add("learn");
+        arrList.add("happy");
+        arrList.add("laugh");
+        arrList.add("peace");
+        arrList.add("happy");
+/**
+ * using for-loop, print the values at even index
+//System.out.println(arr2);
+//Create a method that is able to
+// find/return the string occurring maximum number of times in given ArrayList.
+**/
+
--- src/TestPkg/Testing.java
+++ src/TestPkg/Testing.java
@@ -1,9 +1,11 @@
 package TestPkg;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Testing {
     public static void main(String[] args) {
 
-     String myName = "Maryna Ognev";
+    /** String myName = "Maryna Ognev";
      System.out.println(myName);
 
      int myNameLength = myName.length();
@@ -23,8 +25,47 @@ public class Testing {
      System.out.println( myFirstStatment);
      int myFirstStatmentLength = myFirstStatment.length();
      System.out.println("Length of "+ myFirstStatment + " is "+ myFirstStatmentLength);
-    
 
+**/
+
+
+
+
+
+
+             /** 1. Create an ArrayList variable, which can store Integer type data at each index
+         * 2. Ask user how many values they want to enter/add in the arrayList, that will become length/size of your arrayList
+                *      using scanner, take int value from user
+         * 3. Run a loop, so that user can add values in arrayList (using scanner)
+                *      BUT stop the user as soon user enters the total values
+
+        4. print the arrayList
+              **/
+
+        ArrayList<String> arr1 = new ArrayList<>();
+
+
+            // String[] arr1 = {"20","25","27","18","17","30"};
+        arr1.add("20");
+        arr1.add("25");
+        arr1.add("27");
+        arr1.add("18");
+        arr1.add("17");
+        arr1.add("30");
+              System.out.println("Lenghth off arr: " + arr1.size());
+               Scanner scan = new Scanner(System.in);
+                System.out.println("Please enter the total num value:");
+
+              String userValue = scan. nextLine();
+
+               System.out.print("num" + userValue);
+
+               for (int i = 0; i < arr1.size();i++) {
+              System.out.println(arr1.get(i));
+              }
+              for (int i = 0; i < arr1.size();i+=2) {
+              System.out.println(arr1.get(i));
+              }
     }
 }
 
